43b7887aa210248b008a3541f048a257de99fe50
jfan liu <136590076@qq.com>
2023-12-15 09:59:47 +0800

    Update

--- src/LinguaNex.Application/OpenApi/OpenApiAppService.cs
+++ src/LinguaNex.Application/OpenApi/OpenApiAppService.cs
@@ -2,14 +2,16 @@
 using LinguaNex.Domain;
 using LinguaNex.Dtos;
 using LinguaNex.Entities;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using Wheel.Core.Dto;
 using Wheel.Core.Exceptions;
 using Wheel.Services;
 
 namespace LinguaNex.OpenApi
 {
-    public class OpenApiAppService(IBasicRepository<Culture, string> cultureRepository, IBasicRepository<Projects, string> projectsRepository) : LinguaNexServiceBase, IOpenApiAppService
+    public class OpenApiAppService(IBasicRepository<Culture, string> cultureRepository, IBasicRepository<Projects, string> projectsRepository, IBasicRepository<ProjectAssociation> projectAssociationRepository) : LinguaNexServiceBase, IOpenApiAppService
     {
         public async Task<R<List<ResourcesDto>>> GetResources(string projectId, string? cultureName, bool? all)
         {
@@ -18,6 +20,7 @@ namespace LinguaNex.OpenApi
                 throw new BusinessException(ErrorCode.NotExist);
             if(!project.Enalbe)
                 throw new BusinessException(ErrorCode.NotEnable);
+
             var datas = await cultureRepository.GetListAsync(
                 cultureRepository.BuildPredicate(
                     (true, a => a.ProjectId == projectId),
@@ -26,11 +29,47 @@ namespace LinguaNex.OpenApi
                     ),
                 propertySelectors: a => a.Resources
                 );
-            return Success(datas.Select(a => new ResourcesDto
+            var mainResouces = datas.Select(a => new ResourcesDto
             {
                 CultureName = a.Name,
                 Resources = a.Resources.ToDictionary(b => b.Key, b => b.Value)
-            }).ToList());
+            }).ToList();
+
+
+            var pa = await projectAssociationRepository.SelectListAsync(a => a.MainProjectId == project.Id, a => a.AssociationProjectId);
+            if(pa.Count > 0)
+            {
+                var associationDatas = (await cultureRepository.GetListAsync(
+                cultureRepository.BuildPredicate(
+                    (true, a => pa.Contains(a.ProjectId)),
+                    (!string.IsNullOrWhiteSpace(cultureName), a => a.Name == cultureName),
+                    (!all.HasValue && string.IsNullOrWhiteSpace(cultureName), a => a.Name == CultureInfo.CurrentUICulture.Name)
+                    ),
+                propertySelectors: a => a.Resources
+                )).Select(a => new ResourcesDto
+                {
+                    CultureName = a.Name,
+                    Resources = a.Resources.ToDictionary(b => b.Key, b => b.Value)
+                }).ToList();
+
+                foreach (var associationData in associationDatas)
+                {
+                    var mr = mainResouces.FirstOrDefault(a => a.CultureName == associationData.CultureName);
+                    if (mr != null)
+                    {
+                        var exceptData = associationData.Resources.Keys.Except(mr.Resources.Keys);
+                        if(exceptData.Count() > 0)
+                            mr.Resources = mr.Resources.Union(associationData.Resources.Where(a => exceptData.Contains(a.Key))).ToDictionary(a => a.Key, a => a.Value);
+                    }
+                    else
+                    {
+                        mainResouces.Add(new ResourcesDto { CultureName = associationData.CultureName, Resources = associationData.Resources});
+                    }
+                }
+            }
+            
+
+            return Success(mainResouces);
         }
     }
 }
