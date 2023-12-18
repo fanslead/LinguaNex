d874ec4bda8bea2f779d4f7d800a99d5d3a6b96d
jfan liu <136590076@qq.com>
2023-12-18 16:56:56 +0800

    Update

---
+++ src/LinguaNex.Host/Localization/EFStringLocalizerStore.cs
@@ -0,0 +1,25 @@
+﻿using LinguaNex.Domain;
+using LinguaNex.Entities;
+using Microsoft.Extensions.Localization;
+using System.Globalization;
+using Wheel.DependencyInjection;
+
+namespace Wheel.Localization
+{
+    public class EFStringLocalizerStore(IBasicRepository<Resource, int> resourceRepository)
+        : IStringLocalizerStore, ISingletonDependency
+    {
+        public IEnumerable<LocalizedString> GetAllStrings()
+        {
+            var list = resourceRepository.GetListAsync(r => r.Culture.Name == CultureInfo.CurrentCulture.Name, propertySelectors: r => r.Culture).ConfigureAwait(false).GetAwaiter().GetResult();
+            return list
+                .Select(r => new LocalizedString(r.Key, r.Value, r.Value == null));
+        }
+
+        public string GetString(string name)
+        {
+            var resource = resourceRepository.FindAsync(r => r.Culture.Name == CultureInfo.CurrentCulture.Name).ConfigureAwait(false).GetAwaiter().GetResult();
+            return resource?.Value;
+        }
+    }
+}
