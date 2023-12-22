e8b8e0a6db3a3515c0c0465147b2cdaee1eb47db
FunShow <136590076@qq.com>
2023-12-22 18:10:01 +0800

    Update

--- LinguaNexUI/antd.desgin.pro/config/defaultSettings.ts
+++ LinguaNexUI/antd.desgin.pro/config/defaultSettings.ts
@@ -15,7 +15,7 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'LinguxNex',
   pwa: true,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
