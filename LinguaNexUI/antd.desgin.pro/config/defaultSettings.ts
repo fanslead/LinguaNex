6a1d54d82ab1784e36ca68d05e0c7cd835cec19b
FunShow <136590076@qq.com>
2024-01-03 10:42:01 +0800

    Update

--- LinguaNexUI/antd.desgin.pro/config/defaultSettings.ts
+++ LinguaNexUI/antd.desgin.pro/config/defaultSettings.ts
@@ -17,7 +17,7 @@ const Settings: ProLayoutProps & {
   colorWeak: false,
   title: 'LinguxNex',
   pwa: true,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: '/icons/icon-128x128.png',
   iconfontUrl: '',
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
