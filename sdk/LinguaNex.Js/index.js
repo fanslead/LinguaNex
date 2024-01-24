742eabd3ab491b21333c3764f4b95db94ede0bac
FunShow <136590076@qq.com>
2024-01-24 16:16:03 +0800

    Update

--- sdk/LinguaNex.Js/index.js
+++ sdk/LinguaNex.Js/index.js
@@ -1,10 +1,5 @@
 const linguanex = require('./src/linguanex');
-linguanex.initLinguaNex({
-    baseUrl: 'http://47.119.20.111',
-    locales: ["zh-CN", "en"],
-    defaultLocale: 'zh-CN',
-    project: 'C96755D0-C22C-4DAD-9620-AF64C4C3D9D7'
-})
+
 module.exports = {
     linguanex: linguanex
 };
